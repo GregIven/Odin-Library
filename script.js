3cf6dc013e8afc763a95ade85bbca71ee567ce3c
greg drwe <gdrew21@gmail.com>
2022-01-18 19:48:55 -0800

    added to script: add book (grid and card styled)

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const titleInput = document.getElementById("authorInput");
+const titleInput = document.getElementById("titleInput");
 const authorInput = document.getElementById("authorInput");
 const pagesInput = document.getElementById("pagesInput")
 
@@ -9,13 +9,14 @@ addBookBtn.addEventListener('click', () => addBookToLibrary());
 
 let myLibrary = [];
 
-
-
 function Book(author, title, pages) {
     this.author = author;
     this.title = title;
     this.pages = pages;
 }
+
+const bookProps = Object.getOwnPropertyNames(new Book());
+console.log(bookProps)
 
 function addBookToLibrary() {
     let titleOf = titleInput.value;
@@ -25,5 +26,21 @@ function addBookToLibrary() {
     let bookEntry = new Book(authorOf, titleOf, pagesOf);
 
     myLibrary.push(bookEntry);
+    updateGrid(bookEntry)
     console.log(myLibrary)
+}
+
+function updateGrid(latestBook) {
+    let bookGrid = document.getElementById('booksGrid');
+    let gridItem = document.createElement('div');
+    gridItem.classList.add('book-card');
+
+
+    for (let i = 0; i < bookProps.length; i++) {
+        let pTag = document.createElement('p1');
+        pTag.innerHTML =`<strong>${bookProps[i]}:</strong> ${latestBook[bookProps[i]]}\n`;
+        gridItem.append(pTag);
+    }
+
+    bookGrid.append(gridItem);
 }
